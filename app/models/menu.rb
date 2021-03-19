73938fef32dc82eb9aae8beeec5bbf7351cbb543
David <david.yang026@gmail.com>
2021-03-19 16:31:20 -0700

    updated migration

---
+++ app/models/menu.rb
@@ -0,0 +1,7 @@
+#dn can read, write and query specific entries in the database
+#dn instead of getting back raw SQL, we will get Menu objects
+class Menu < ApplicationRecord
+
+
+
+end
---
+++ db/migrate/20210319211041_create_menus.rb
@@ -0,0 +1,10 @@
+class CreateMenus < ActiveRecord::Migration[6.0]
+  def change
+    create_table :menus do |t|
+      #dn t.(specify datatype)
+      #dn :label would be either breakfast, lunch or dinner
+      #dn null: false -> forces an entry, so every menu has to have a label
+      t.string :label, null: false
+    end
+  end
+end
