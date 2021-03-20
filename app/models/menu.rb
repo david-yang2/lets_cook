6f7f761d7fc0dc98be90298b33a74fdc85b4d3d6
David <david.yang026@gmail.com>
2021-03-20 11:30:28 -0700

    changed model

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -1,7 +1,20 @@
 #dn can read, write and query specific entries in the database
 #dn instead of getting back raw SQL, we will get Menu objects
+
+#dn for every column that exists, rails will create getter and setter for you
 class Menu < ApplicationRecord
 
+  #dn instead of throwing a long error message from SQL when input label is incorrect
+    #dn the error will come from our model which is a lot shorter
+  validates :label, presence: true
 
+  #dn checks to see if the label is valid
+  validate :check_label
 
+  def check_label
+    #dn if label is not one of the options in the array, throw an error
+    unless ["breakfast", "lunch", "dinner"].include? self.label
+      error[:name] << "Please pick a correct label for the menu"
+    end
+  end
 end
