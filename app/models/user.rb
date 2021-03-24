e64f04299b00f43df0b1c8a96b1de56f918832ef
David <david.yang026@gmail.com>
2021-03-24 13:39:16 -0700

    password= function

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ApplicationRecord
+    def password=(password)
+
+        #dn instead of saving actual password
+            #dn use BCrypt to hash it and save it to password_digest column in DB
+            #dn since we dont have a password column in DB and we don't actually
+            #dn want to save the raw password
+        self.password_digest = BCrypt::Password.create(password)
+    end
+
+    
 end
