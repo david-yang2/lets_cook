7adf86868a1202fa9a97e691dc26d3897049fa50
David <david.yang026@gmail.com>
2021-03-24 13:12:59 -0700

    user table migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_173709) do
+ActiveRecord::Schema.define(version: 2021_03_24_195050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_03_23_173709) do
   create_table "menus", force: :cascade do |t|
     t.string "chefs_name", null: false
     t.string "location", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
