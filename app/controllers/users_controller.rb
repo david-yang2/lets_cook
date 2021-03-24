be36c95642ac05cfa095cee236f286f630aacb7d
David <david.yang026@gmail.com>
2021-03-24 13:59:51 -0700

    created create and new actions

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+    def create
+        @user = User.new(user_params)
+
+        if @user.save
+            render json: @user
+        else 
+            render json: @user.errors.full_messages
+        end
+    end
+
+
+    def new
+        @user = User.new
+    end
+
+    private
+
+    def user_params
+        #dn notice we are calling .permit with :password, NOT password_digest
+        #dn mass-assignment will call the setter method password=
+        params.require(:user).permit(:username, :password)
+    end
+end
