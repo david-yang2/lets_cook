ad361fe8ddd61414423e94d3eee23fae39d8ee1e
David <david.yang026@gmail.com>
2021-03-24 14:40:56 -0700

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
+
+    #dn validation will call this to check the attribute
+    attr_reader :password
+
+    validates :username, presences: true
+
+    #if password is bank send that message
+    validates :password_digest, presence: { message: 'Password can\'t be blank'}
+
+    #dn make sure password is atleast 6 characters long
+    #dn allow_nil: true means:
+        #dn validation will pass if @password is nil (desirable b/c:
+            #dn @password attribute is only set IF WE CHANGE THE PASSWORD WITH #password=)
+    validates :password, length: {minimum: 6, allow_nil:true}
+
+
     def password=(password)
 
+        #dn store password in an instance variable but never save this to DB
+            #dn @password will not be persisted
+        @password = password
         #dn instead of saving actual password
             #dn use BCrypt to hash it and save it to password_digest column in DB
             #dn since we dont have a password column in DB and we don't actually
