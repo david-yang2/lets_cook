b231ca35b11fb0522e43845d23e4b67ed7037c8c
David <david.yang026@gmail.com>
2021-03-31 18:12:24 -0700

    updated menus/index

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,26 @@
+const path = require('path'); 
+
 module.exports = {
-    context: __dirname,
-    // expects our entry is app.js from frontend folder
-        // app.js is just to get us started
-    entry: __dirname + "./frontend/app.js",
-    output: {
-        path:__dirname + "/app/assets/javascripts",
-        filename: "bundle.js"
-    }
-}
+  entry: './frontend/entry.jsx',
+  output: {   
+    path: path.resolve(__dirname),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  module: {
+  rules: [{  
+        test: /\.jsx?$/, 
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'babel-loader', 
+          options: { 
+            presets: ['@babel/env', '@babel/react'] 
+          }
+        },
+      }
+    ]
+  }
+};
