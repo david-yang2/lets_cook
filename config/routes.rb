4275c392bf9cb8239644eb2c04f7c4f44ebbc212
David <david.yang026@gmail.com>
2021-04-02 09:18:06 -0700

    updated namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 Rails.application.routes.draw do
   root to: "static_pages#root"
   
-  resources :menus do
-    resources :items, only: [:index, :new]
+  namespace :api, defaults: {format: :json} do
+    resources :items, only: [:create, :show, :update, :destory, :edit]
+
+    resources :users
+
+
+    resource :session, only: [:create, :destroy]
+    resources :menus do
+      resources :items, only: [:index, :new]
+    end
+
+
   end
-
-  resources :items, only: [:create, :show, :update, :destory, :edit]
-
-  resources :users
-
-
-  resource :session, only: [:create, :destroy]
   
 end
