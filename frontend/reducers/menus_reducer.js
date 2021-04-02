809b8d1ebc88cae64800c019825c924b7053414f
David <david.yang026@gmail.com>
2021-04-02 12:04:07 -0700

    created MenusReducer

---
+++ frontend/reducers/menus_reducer.js
@@ -0,0 +1,30 @@
+import {
+    RECEIVE_ALL_MENUS,
+    RECEIVE_MENU,
+    REMOVE_MENU,
+} from '../actions/menu_actions';
+
+
+const MenuReducer = (oldstate, action) =>{
+    Object.freeze(oldState);
+
+
+    let nextState = Object.assign({}, oldState);
+
+    switch (action.type) {
+        case: RECEIVE_ALL_MENUS:
+            return action.menus;
+
+        case: RECEIVE_MENU:
+            nextState[action.menu.id = action.post];
+            return nextState;
+
+        case: REMOVE_MENU:
+            delete nextState(action.id);
+            return nextState;
+        default:
+            return oldState;
+    }
+}
+
+export default MenusReducer;
