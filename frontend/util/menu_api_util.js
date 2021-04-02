337a49744428e38b6f5d2d2f4165345346e24963
David <david.yang026@gmail.com>
2021-04-02 10:18:47 -0700

    initial commit

---
+++ frontend/util/menu_api_util.js
@@ -0,0 +1,37 @@
+export const fetchMenus = () => {
+    return $.ajax({
+        url:'/api/menus'
+        method: "GET"
+    });
+};
+
+export const fetchMenus = id => {
+    return $.ajax({
+        url:'/api/menus/${id}',
+        method: "GET"
+    });
+};
+
+
+export const createMenus = menu => {
+    return $.ajax({
+        url:'/api/menus',
+        method: "POST",
+        data: {menu: menu}
+    })
+};
+
+export const updateMenu = menu => {
+    return %.ajax({
+        url:'/api/menus/${menu.id}',
+        method: "POST"
+        data: {menu}
+    })
+};
+
+export const deleteMenu = id => {
+    return $.ajax({
+        url: '/api/menus/${menu.id}',
+        method: "DESTROY"
+    })
+};
