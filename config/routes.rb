5d1ae48a883f777495249033d68027e83ac887e3
David <david.yang026@gmail.com>
2021-04-01 22:00:42 -0700

    rerouted root to static_pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,15 @@
 Rails.application.routes.draw do
-
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: "static_pages#root"
+  
   resources :menus do
-
-    #dn these are the routes you want to be nested under menus
-      #dn e.g. /menus/:menus_id/items
     resources :items, only: [:index, :new]
   end
 
-  #dn make some top level resources for items
-    #dn these actions will work with /items/:id
-  #dn when you are creating, showing, updating, destory a SINGLE item,
-    #dn it makes more sense that is on the top-level
   resources :items, only: [:create, :show, :update, :destory, :edit]
 
   resources :users
 
-  #dn no Session model class, nor sessions table
-  #dn session resource is singular because the user will only use at most one session: their own
+
   resource :session, only: [:create, :destroy]
-
-
-  #dn instead of the default Rails page, redirect us /menus
-  #dn essentiall changing the root too /menus
-  root to: redirect("/menus")
+  
 end
