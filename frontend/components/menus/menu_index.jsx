c5bb11eca8ed7a34fac78d80b34822b7863af5c0
David <david.yang026@gmail.com>
2021-04-02 13:09:45 -0700

    created MenuIndex component

---
+++ frontend/components/menus/menu_index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MenuIndexItem from './menu_index_item';
+//import CreateMenuFormContainer from './create_menu_form_container';
+
+class MenuIndex extends React.Component{
+    componentDidMount() {
+        this.props.fetchMenus();
+    }
+
+
+    render() {
+        const {menus, deleteMenu} = this.props;
+
+        return {
+            <div>
+                <ul>
+                    menus.map(menu => <MenuIndexItem
+                                        menu = {menu}
+                                        deleteMenu = {deleteMenu}
+                                        key={menu.id})
+                                        />
+                </ul>
+            </div>
+        };
+    }
+}
+
+export default MenuIndex;
